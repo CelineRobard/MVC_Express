ffb660cb8a1d55d090ce7d35447dd0be8bd377be
RobardCeline <celine.robard@gmail.com>
2021-12-17 14:44:12 -0500

    début de solution pour la modification de l image sur le formulaire de modification

--- routes/usagers.js
+++ routes/usagers.js
@@ -14,11 +14,83 @@ router.get('/logout', (requete, reponse) => {
     requete.flash('succes_msg', 'Deconnexion reussie');
     reponse.redirect('/usagers/login');
 });
-router.get('/register', estAuthentifie, estAdmin, (requete, reponse) => {
+router.get('/ajouter', estAuthentifie, estAdmin, (requete, reponse) => {
     console.log(requete.body);
     reponse.render('afficheUsager',{
         titre: 'Créer',
-        iconeFAS: 'fa-user-plus'
+        iconeFAS: 'fa-user-plus',
+        usager: requete.user
+    });
+});
+
+router.get('/editer/:idUsager', estAuthentifie, estAdmin, (requete, reponse) => {
+    Usagers.findById(requete.params.idUsager).then(usager => {
+
+        const { _id,nom,email,fichierImage } = usager;
+        const admin = usager.roles.find(role => role === "admin");
+        const gestion = usager.roles.find(role => role === "gestion");
+
+        reponse.render('afficheUsager',{
+            titre: 'Modifier',
+            iconeFAS: 'fa-user-edit',
+            action: '/usagers/editer/:idUsager',
+            _id,
+            nom,
+            email,
+            admin,
+            gestion,
+            fichierImage
+        });
+
+    }).catch(err => console.log(err));
+
+});
+
+/******************************************************************************************************/
+router.get('/editer/:idUsager/image', estAuthentifie, estAdmin, (requete, reponse) => {
+    let test="passer un parametre";
+    reponse.status(200).send(`ici on va modifier l image 
+    <form action="/usagers/editer/${requete.params.idUsager}/imageModifie" method="POST"  enctype="multipart/form-data">
+        ${test}
+        <input type="text" id="nom" name="nom" value="test image" hidden/>
+        <input type="text" id="email" name="email"  value="test@image.com" hidden />
+        <input type="checkbox" id="admin" name="admin" hidden />
+        <input type="checkbox" id="gestion" name="gestion" hidden />
+        <input type="password" id="password" name="password" value="123456" hidden />
+        <input type="text" id="fichierImage" name="fichierImage" value="/images/ca6cf45fa3072dfb8e5b6a3fdcc5cb53" hidden />
+        <div class="form-group">
+            <label>Téléverser</label>
+            <input type="file" name="fichier" required />
+        </div>
+        <button type="button" class="btn btn-secondary btn-block">Annuler</button>
+        <button type="submit" class="btn btn-primary btn-block">Confirmer l ajout</button>
+    </form>`);
+});
+
+router.post('/editer/:idUsager/imageModifie', estAuthentifie, estAdmin, (requete, reponse) => {
+    //console.log(requete.query);
+    //console.log(requete.files);
+    //console.log(requete.body);
+    const { nom, email, password, password2, admin, gestion, normal } = requete.body;
+    const { originalname,destination,filename,size,path,mimetype} = requete.files[0];
+    const fichierImage = conserverFichier(path,filename);
+    reponse.render('afficheUsager',{
+        titre: 'Modifier',
+        iconeFAS: 'fa-user-edit',
+        _id: requete.params.idUsager,
+        nom,
+        email,
+        password,
+        fichierImage
+    });
+});
+/******************************************************************************************************/
+
+router.get('/supprimer/:idUsager', estAuthentifie, estAdmin, (requete, reponse) => {
+    console.log(requete.body);
+    reponse.render('afficheUsager',{
+        titre: 'Supprimer',
+        iconeFAS: 'fa-user-slash'
     });
 });
 router.post('/login', (requete, reponse, next) => {
@@ -56,7 +128,7 @@ function conserverFichier(path,filename){
     return filename;
 }
 
-router.post('/register', estAuthentifie, estAdmin, (requete, reponse) => {
+router.post('/ajouter', estAuthentifie, estAdmin, (requete, reponse) => {
     // console.log(requete.body);
     const { nom, email, password, password2, admin, gestion, normal } = requete.body;
     const { originalname,destination,filename,size,path,mimetype} = requete.files[0]; //retourne un tableau de fichiers (comme nous on sait qu'on en accepte un seul on va le chercher directement en position 1)
@@ -140,7 +212,7 @@ router.post('/register', estAuthentifie, estAdmin, (requete, reponse) => {
                             requete.flash(
                                 'succes_msg', 'Cet usager vient d\'être inséré dans la BD et vous pouvez enregistrer un autre usager'
                             );
-                            reponse.redirect('/usagers/register');
+                            reponse.redirect('/usagers/ajouter');
                         })
                         .catch(err => console.log(err));
                     });
@@ -154,7 +226,7 @@ router.get('/listeUsagers', estAuthentifie, (requete, reponse) => {
     Usagers.find({
         //query vide
     }).then((tousUsagers) => {
-        console.log(tousUsagers);
+        //console.log(tousUsagers);
         reponse.render('listeUsagers',{tousUsagers});
     }).catch( err => console.log(err));
 });
